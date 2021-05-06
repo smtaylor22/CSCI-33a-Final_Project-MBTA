e041ee6d9ff50a27929e40507c1f0f15c685a97c
Stephen Taylor <stephen@FBISURVANCEVAN3.sattler.local>
2021-05-06 17:38:33 -0400

    worked on script.js and models.py

--- MBTA/arrival_time/models.py
+++ MBTA/arrival_time/models.py
@@ -5,4 +5,26 @@ from django.db import models
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    tracking = models.ManyToManyField("Station", related_name="trackers")
+
+class Station(models.Model):
+    # MBTA stop id associated with stop ---- so can querry for this stop prediction later 
+    mbta_id = models.IntegerField()
+    # Stop name - Name of the stop the user is tracking
+    name = models.CharField(max_length=255)
+    # Associated line with Stop (eg. Subway: Red line, Bus: 70)
+    line = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return f"{self.name} - {self.line}"
+
+# Comments - User comments and info associated with a station/stop
+class Comment(models.Model):
+    # Text for comments - removes requirement for comments
+    text = models.TextField(blank=True)
+    author = models.ForeignKey("User", on_delete=models.CASCADE, related_name="authors")
+    created = models.DateTimeField(auto_now_add=True)
+    station = models.ForeignKey("Station", on_delete=models.CASCADE, related_name="comments")
+    
+    def __str__(self):
+        return self.content
--- MBTA/arrival_time/static/arrival_time/script.js
+++ MBTA/arrival_time/static/arrival_time/script.js
@@ -6,9 +6,20 @@ document.addEventListener('DOMContentLoaded', function() {
     .then(response => response.json())
     .then(data => {
         // Log data to the console
+        console.log(data.data.map(item => item.attributes.arrival_time));
+        const train = data.data[3];
+        const time = train.attributes.arrival_time;
+        const t = new Date(time);
+
+        // Learned how to subtract time of two dates in Javascript here: https://bearnithi.com/2019/11/10/how-to-calculate-the-time-difference-days-hours-minutes-between-two-dates-in-javascript/
+        const prediction = Math.abs(t) - Math.abs(new Date());
+        console.log(t);
+        console.log(prediction/60000);
+        console.log(Math.floor(prediction/60000));
         console.log(data);
-        const test = data.data.attributes;
-        console.log(test);
+
+        
+        document.querySelector('#arrival-time').innerHTML = time
     });
 
 });
